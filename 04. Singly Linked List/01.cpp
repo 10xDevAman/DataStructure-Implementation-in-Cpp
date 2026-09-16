af731e45f6a5d8718d2d751380493d20c02ad3f2
Aman Shah <10xdevaman@gmail.com>
2026-09-16 18:36:52 +0530

    Program 1 : Define SLL class with start pointer.

---
+++ 04. Singly Linked List/01.cpp
@@ -0,0 +1,27 @@
+// Define a class SLL to implement singly linked list data structure with member variable start pointer of type node.
+
+
+#include <iostream>
+using namespace std;
+
+// Node structure
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+// Singly Linked List
+class SLL
+{
+private:
+    Node *start;
+};
+
+int main()
+{
+    SLL list;
+
+    return 0;
+}
