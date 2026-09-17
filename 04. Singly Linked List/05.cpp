f107b35ff74b0ac37449e271e3f41a6331661840
Aman Shah <10xdevaman@gmail.com>
2026-09-17 22:16:44 +0530

    Program 5 : Add search method to SLL.

---
+++ 04. Singly Linked List/05.cpp
@@ -0,0 +1,94 @@
+// In question 1, define a method to search a node with the give item.
+
+
+#include <iostream>
+using namespace std;
+
+// Node
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+// Singly Linked List
+class SLL
+{
+private:
+    Node *start;
+
+public:
+    // Constructor
+    SLL()
+    {
+        start = NULL;
+    }
+
+    // Insert data at beginning
+    void insertAtBeginning(int value)
+    {
+        Node *newNode = new Node;
+
+        newNode->data = value;
+        newNode->next = start;
+
+        start = newNode;
+    }
+
+    // Insert data at end
+    void insertAtEnd(int value)
+    {
+        Node *newNode = new Node;
+
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (start == NULL)
+        {
+            start = newNode;
+            return;
+        }
+
+        Node *temp = start;
+
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+    // Search an item
+    int search(int item)
+    {
+        Node *temp = start;
+        int index = 0;
+
+        while (temp != NULL)
+        {
+            if (temp->data == item)
+                return index;
+
+            temp = temp->next;
+            index++;
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    SLL list;
+
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+    list.insertAtEnd(40);
+
+    cout << "Index: " << list.search(30);
+
+    return 0;
+}
