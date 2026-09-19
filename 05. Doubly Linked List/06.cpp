58dbc878fb77b8e76cf9710b81f613b1c11e01c5
Aman Shah <10xdevaman@gmail.com>
2026-09-19 22:06:43 +0530

    Program 6 : Add insert after node to DLL.

---
+++ 05. Doubly Linked List/06.cpp
@@ -0,0 +1,124 @@
+// In question 1, define a method to insert a data into the list after the specified node of the list.
+
+
+#include <iostream>
+using namespace std;
+
+// Node
+class Node
+{
+public:
+    Node *prev;
+    int data;
+    Node *next;
+};
+
+// Doubly Linked List
+class DLL
+{
+private:
+    Node *start;
+
+public:
+    // Constructor
+    DLL()
+    {
+        start = NULL;
+    }
+
+    // Insert data at beginning
+    void insertAtBeginning(int value)
+    {
+        Node *newNode = new Node;
+
+        newNode->prev = NULL;
+        newNode->data = value;
+        newNode->next = start;
+
+        if (start != NULL)
+        {
+            start->prev = newNode;
+        }
+
+        start = newNode;
+    }
+
+    // Insert data at end
+    void insertAtEnd(int value)
+    {
+        Node *newNode = new Node;
+
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (start == NULL)
+        {
+            newNode->prev = NULL;
+            start = newNode;
+            return;
+        }
+
+        Node *temp = start;
+
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        newNode->prev = temp;
+        temp->next = newNode;
+    }
+
+    // Search an item
+    int search(int item)
+    {
+        Node *temp = start;
+        int index = 0;
+
+        while (temp != NULL)
+        {
+            if (temp->data == item)
+                return index;
+
+            temp = temp->next;
+            index++;
+        }
+
+        return -1;
+    }
+
+    // Insert data after specified node
+    void insertAfter(Node *node, int value)
+    {
+        if (node == NULL)
+            return;
+
+        Node *newNode = new Node;
+
+        newNode->data = value;
+
+        // Connect new node with next node
+        newNode->next = node->next;
+        newNode->prev = node;
+
+        // Update next node's prev
+        if (node->next != NULL)
+        {
+            node->next->prev = newNode;
+        }
+
+        // Connect specified node with new node
+        node->next = newNode;
+    }
+};
+
+int main()
+{
+    DLL list;
+
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+
+    return 0;
+}
